6346fa94277fb0e171e87fcb010fc0bf111b5339
mauveMoon <15255146296@163.com>
2018-02-19 15:45:20 +0800

    Create 六度空间.cpp

---
+++ 六度空间.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+#include<cstdio>
+using namespace std;
+
+class Graph{
+public:
+	int nNode;
+	vector<vector<bool>> edge;
+	Graph(int n){
+		nNode = n;
+		for(int i=0;i<n;i++){
+			vector<bool> t(n, 0);
+			edge.push_back(t);
+		}
+	}
+	void add_edge(int v1, int v2){
+		edge[v1][v2] = 1;
+		edge[v2][v1] = 1;
+	}
+	
+	int six_degree(int v){
+		vector<int> nodes(nNode, 0);
+		deque<int> bfs;
+		bfs.push_back(v);
+		nodes[v] = 1;
+		int record = 1;
+		
+		while(!bfs.empty()){
+			int top = bfs.front();
+			bfs.pop_front();
+			if(nodes[top] == 7) break;
+			for(int i=0;i<nNode;i++){
+				if(edge[top][i] == 1 && nodes[i] == 0){
+					bfs.push_back(i);
+					record ++ ;
+					nodes[i] = nodes[top] + 1;
+				}
+			}
+		}
+		return record;
+	}
+};
+
+int main(){
+	int N, M;
+	cin >> N >> M;
+	Graph g(N+1);
+	int v1, v2;
+	for(int i=0;i<M;i++){
+		cin >> v1 >> v2;
+		g.add_edge(v1, v2);
+	}
+	for(int i=1;i<=N;i++){
+		int rec = g.six_degree(i);
+		printf("%d: %.2f%%\n", i, rec*100.0/N);
+	}
+	return 0;
+}
